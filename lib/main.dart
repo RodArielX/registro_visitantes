1222935ee0983139fdc7b72e2b51c08c68f1f959
RodArielX <151254192+RodArielX@users.noreply.github.com>
2025-07-15 16:12:48 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://lossjztnhdpcghpyaayd.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxvc3NqenRuaGRwY2docHlhYXlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MDcxNTIsImV4cCI6MjA2ODE4MzE1Mn0.WO9NwY6buQJtyWp-panMHz700B5lvaULH4J2d72qfj4',
+    url: 'url_supabase',
+    anonKey: 'anon_supabase',
   );
 
   runApp(MyApp());
